199eee5005fc5ddaac66b491070505c660245047
sddrouet <sddrouet@espe.edu.ec>
2025-02-26 19:14:38 -0500

    kubernetes update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-const IP_BACKEND = 'http://172.174.4.92';
+const IP_BACKEND_CURSOS = 'http://micro-cursos-app:8003';
+const IP_BACKEND_ESTUDIANTES = 'http://micro-estudiante:8002';
 
-const BASE_URL_ESTUDIANTES = `${IP_BACKEND}:8002/api/estudiantes`;
-const BASE_URL_CURSOS = `${IP_BACKEND}:8003/api/cursos`;
-const BASE_URL_RELACION = `${IP_BACKEND}:8003/api/curso-estudiante`;
+const BASE_URL_ESTUDIANTES = `${IP_BACKEND_ESTUDIANTES}/api/estudiantes`;
+const BASE_URL_CURSOS = `${IP_BACKEND_CURSOS}/api/cursos`;
+const BASE_URL_RELACION = `${IP_BACKEND_CURSOS}/api/curso-estudiante`;
 
 export const getEstudiantes = () => axios.get(BASE_URL_ESTUDIANTES);
 export const getCursos = () => axios.get(BASE_URL_CURSOS);
